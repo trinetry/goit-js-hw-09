349b768a5970bd7db1aea7b23ed276354bcd459b
trinetry <stro2007@ukr.net>
2021-11-06 16:16:10 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"'),
+    stopBtn: document.querySelector('button[data-action="stop"'),
+  },
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
